2776d12ea1596ccff1498a00e22dfbbf7398f8bc
deenmd786 <deen8851@gmail.com>
2026-02-18 22:51:41 +0530

    maid changes

--- server.js
+++ server.js
@@ -27,6 +27,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Something went wrong!" });
 });
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
